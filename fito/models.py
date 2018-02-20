9454058046117a86c107a0034b1717707e2c87a6
denoiser <ASBiryukov@omsk.luxoft.com>
2018-02-20 08:53:26 +0300

    modified:   fit/settings.py 	new file:   fito/__init__.py 	new file:   fito/admin.py 	new file:   fito/apps.py 	new file:   fito/migrations/__init__.py 	new file:   fito/models.py 	new file:   fito/tests.py 	new file:   fito/urls.py 	new file:   fito/views.py

---
+++ fito/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils import timezone
+
+#Описание упр
+class Exercise(models.Model):
+	group = models.CharField(max_length=150)
+	title = models.CharField(max_length=40)
+	text = models.TextField()
+	# anim = models.ImageField
+#Прогрес
+class Progress(models.Model):
+
+
+	
+
