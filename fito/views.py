f733b5d63ede4bd436907b8a284d64bf4946ed17
denoiser <ASBiryukov@omsk.luxoft.com>
2018-02-22 18:36:02 +0300

    new file:   fito/templates/fito/base.html 	new file:   fito/templates/fito/wrapper.html 	modified:   fito/views.py

--- fito/views.py
+++ fito/views.py
@@ -4,4 +4,4 @@ from django.utils import timezone
 
 def excercise(request):
 	excercise = Excercise.objects.order_by('group')
-	return render(request, 'fito/excercise_list.html', {'excercise':excercise})
+	return render(request, 'fito/base.html', {'excercise':excercise})
