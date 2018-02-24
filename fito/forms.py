e26595db4d147aa0d9bd929005f9b9035897d349
deNoi5e <denoise@ya.ru>
2018-02-25 03:18:41 +0600

    изменено:      fito/forms.py 	изменено:      fito/models.py 	изменено:      fito/templates/fito/excercise_list.html 	изменено:      fito/urls.py 	изменено:      fito/views.py 	удалено:       fito/widgets.py

--- fito/forms.py
+++ fito/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 from .models import Excercise
 
-class ExcerciseForm(forms.ModelForm):
 
+class ExcerciseForm(forms.ModelForm):
     class Meta:
         model = Excercise
         fields = ('group', 'title', 'text',)
--- fito/views.py
+++ fito/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Excercise
 from django.utils import timezone
 
+
 def excercise(request):
-	excercises = get_object_or_404(Excercise)
-	return render(request, 'fito/excercise_list.html', {'excercise': excercises})
+    excercises = get_object_or_404(Excercise)
+    link_url = 'fito/excercise_list.html'
+    return render(request, link_url, {'excercise': excercises})
