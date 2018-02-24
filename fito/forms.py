95dab5b58794abe48ddd4d1a82f5a4841d762dfe
deNoi5e <denoise@ya.ru>
2018-02-24 20:49:25 +0600

    изменено:      fito/forms.py

--- fito/forms.py
+++ fito/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from bootstrap_datepicker.widgets import DatePicker
 from .models import Excercise
 
 class ExcerciseForm(forms.ModelForm):
@@ -7,15 +6,3 @@ class ExcerciseForm(forms.ModelForm):
     class Meta:
         model = Excercise
         fields = ('group', 'title', 'text',)
-
-class ToDoForm(forms.Form):
-    todo = forms.CharField(
-        widget=forms.TextInput(attrs={"class": "form-control"}))
-    date = forms.DateField(
-        widget=DatePicker(
-            options={
-                "format": "mm/dd/yyyy",
-                "autoclose": True
-            }
-        )
-    )
